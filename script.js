d0adb6d6a4053b5ebccde112d9f5ae0a671be49c
vanessawin <vanessa.win@hotmail.com>
2022-05-13 16:26:23 -0300

    validaçao cpf 5

--- script.js
+++ script.js
@@ -9,35 +9,48 @@ function validaCpf(cpf) {
         var digitos = cpf.substring(9);
         // console.log("Numeros do cpf " + numeros);
         // console.log("Digitos do cpf " + digitos);
-
+        //37142811854
         var soma = 0
         for (var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
         }
-        console.log(soma);
-
+        //console.log(soma);
         var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
-
         //validaçao do meu primeiro digito
         if (resultado != digitos.charAt(0)) {
             return false
         }
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+        //console.log(soma);
         // console.log(digitos.toString().charAt(0) + " é a primeira posição da variavel soma")
+
+        //validação do 2 digito
+        resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+        if (resultado != digitos.charAt(1)) {
+            return false;
+        }
         return true
     }
 }
 
 function validacao() {
     console.log("Iniciando validação CPF");
-
+    document.getElementById("success").style.display = "none";
+    document.getElementById("error").style.display = "none";
     var cpf = document.getElementById("cpf_digitado").value
 
     var resultadoValicacao = validaCpf(cpf);
 
     if (resultadoValicacao) {
         document.getElementById("success").style.display = "block";
+
     } else {
-        document.getElementById("error").style.display = "block";
+        document.getElementById("error").style.display = "block"
     }
 
 }
