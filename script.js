075f71aa57e54a4e137a09c20469fe614ff76ed1
vanessawin <vanessa.win@hotmail.com>
2022-05-13 12:38:55 -0300

    validaçao cpf 2

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 console.log("javascript carregado");
 
+function validaCpf(cpf) {
+    return false;
+}
+
 function validacao() {
     console.log("Iniciando validação CPF");
+
+    var cpf = document.getElementById("cpf_digitado").value
+
+    var resultadoValicacao = validaCpf(cpf);
+
+    if (resultadoValicacao) {
+        document.getElementById("success").style.display = "block";
+ } else {
+     document.getElementById("error").style.display = "block";
+ }
+
 }
